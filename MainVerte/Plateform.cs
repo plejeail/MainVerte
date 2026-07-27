745d639ca01b77f487113f25b98e52bb9d78e34b
plejeail <pierre.lejeail@hotmail.fr>
2026-07-28 00:14:26 +0200

    Debug utils and logs

---
+++ MainVerte/Plateform.cs
@@ -0,0 +1,59 @@
+﻿namespace MainVerte;
+
+
+using System;
+
+#if ANDROID
+using Android.App;
+#endif
+
+static class Platform
+{
+    public static void LogError(string message) {
+#if ANDROID
+        Android.Util.Log.Error("MainVerte", message);
+#else
+        Console.WriteLine($"ERR: MainVerte: {message}");
+#endif
+    }
+
+    public static void LogWarning(string message) {
+#if ANDROID
+        Android.Util.Log.Warn("MainVerte", message);
+#else
+        Console.WriteLine($"WRN: MainVerte: {message}");
+#endif
+    }
+
+    public static void LogInfo(string message) {
+#if ANDROID
+        Android.Util.Log.Info("MainVerte", message);
+#else
+        Console.WriteLine($"INF: MainVerte: {message}");
+#endif
+    }
+
+    public static void LogVerbose(string message) {
+#if ANDROID
+        Android.Util.Log.Verbose("MainVerte", message);
+#else
+        Console.WriteLine($"VER: MainVerte: {message}");
+#endif
+    }
+
+    public static void LogDebug(string message) {
+#if ANDROID
+        Android.Util.Log.Debug("MainVerte", message);
+#else
+        Console.WriteLine($"DBG: MainVerte: {message}");
+#endif
+    }
+
+    public static string ApplicationPath() {
+#if ANDROID
+        return Application.Context.FilesDir?.AbsolutePath ?? String.Empty;
+#else
+        return ".";
+#endif
+    }
+}
---
+++ MainVerte/Utils.cs
@@ -0,0 +1,106 @@
+﻿namespace MainVerte;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+static class Require
+{
+    [Conditional("DEBUG")]
+    public static void True(bool condition, [CallerArgumentExpression(nameof(condition))] string? expression = null) {
+        if (!condition) {
+            Debug.Fail($"Precondition failed: {expression}");
+        }
+    }
+
+    [Conditional("DEBUG")]
+    public static void NotNull<T>(T? value, [CallerArgumentExpression(nameof(value))] string? expression = null) {
+        if (value == null) {
+            Debug.Fail($"Precondition failed: {expression} is null");
+        }
+    }
+
+    [Conditional("DEBUG")]
+    public static void NotEmpty(string? value,
+                                [CallerArgumentExpression(nameof(value))] string? expression = null)
+    {
+        if (String.IsNullOrEmpty(value)) {
+            Debug.Fail($"Precondition failed: {expression} must not be null or empty.");
+        }
+    }
+
+    [Conditional("DEBUG")]
+    public static void NotEmpty<T>(IReadOnlyCollection<T>? value,
+                                   [CallerArgumentExpression(nameof(value))] string? expression = null)
+    {
+        NotNull(value);
+        if (value!.Count == 0) {
+            Debug.Fail($"Precondition failed: {expression} must not be empty.");
+        }
+    }
+}
+
+static class Ensure
+{
+
+    [Conditional("DEBUG")]
+    public static void True(bool condition, [CallerArgumentExpression(nameof(condition))] string? expression = null) {
+        if (!condition) {
+            Debug.Fail($"Postcondition failed: {expression}");
+        }
+    }
+
+    [Conditional("DEBUG")]
+    public static void NotNull<T>(T? value, [CallerArgumentExpression(nameof(value))] string? expression = null) {
+        if (value == null) {
+            Debug.Fail($"Postcondition failed: {expression} is null");
+        }
+    }
+
+    [Conditional("DEBUG")]
+    public static void NotEmpty(string? value,
+                                [CallerArgumentExpression(nameof(value))] string? expression = null)
+    {
+        if (String.IsNullOrEmpty(value)) {
+            Debug.Fail($"Precondition failed: {expression} must not be null or empty.");
+        }
+    }
+
+    [Conditional("DEBUG")]
+    public static void NotEmpty<T>(IReadOnlyCollection<T>? value,
+                                   [CallerArgumentExpression(nameof(value))] string? expression = null)
+    {
+        NotNull(value);
+        if (value!.Count == 0) {
+            Debug.Fail($"Precondition failed: {expression} must not be empty.");
+        }
+    }
+}
+
+static class Log
+{
+    public static void Info(string message) {
+        Platform.LogInfo(message);
+    }
+
+    public static void Verbose(string message) {
+        Platform.LogVerbose(message);
+    }
+
+    public static void Debug(string message) {
+        Platform.LogDebug(message);
+    }
+
+    public static void Warn(string message) {
+        Platform.LogWarning(message);
+    }
+
+    [DoesNotReturn]
+    public static void Error(string message) {
+        Platform.LogError(message);
+        throw new InvalidOperationException(message);
+    }
+}
